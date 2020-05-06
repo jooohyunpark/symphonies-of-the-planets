678696f5aa190a73a6811bd56d89c842557b2743
Joohyun Park <jhp527@nyu.edu>
2020-05-05 22:08:39 -0400

    setup

---
+++ components/panel.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="panel"></div>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.panel {
+  width: 100px;
+  height: 100px;
+}
+</style>
