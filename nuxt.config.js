d46ff470364da22a9e38626f81e18140ace79899
Joohyun Park <jhp527@nyu.edu>
2020-06-27 21:43:53 -0400

    add social tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Symphonies of the Planets',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -12,6 +12,38 @@ export default {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
+      },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content: 'generative music, generative music space'
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://symphonies-of-the-planets.parkjoohyun.com/'
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Symphonies of the Planets'
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: process.env.npm_package_description || ''
+      },
+      { hid: 'og:image', property: 'og:image', content: '' },
+      {
+        hid: 'twitter:card',
+        property: 'twitter:card',
+        content: 'summary_large_image'
+      },
+      {
+        hid: 'twitter:image',
+        property: 'twitter:image',
+        content: ''
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
